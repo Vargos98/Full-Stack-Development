e2cac6a78a2d6f0f99efb3b6362d2394b1ed7191
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-18 08:28:39 +0530

    Update levelUpChallenges.js

--- HTML-CSS-JS/levelUpChallenges.js
+++ HTML-CSS-JS/levelUpChallenges.js
@@ -105,7 +105,22 @@ let worldCities = {
 
    */
 
+let worldCities = {
+  "Sydney": 5000000,
+  Tokyo: 9000000,
+  Berlin: 3500000,
+  Paris: 2200000,
+};
 
+let largeCities = {};
+
+for( const city in worldCities ){
+  if( worldCities[city] < 3000000){
+    continue;
+  }
+  largeCities[city] = worldCities[city];
+}
+console.log(largeCities);
 
 
 // let worldCities = {
